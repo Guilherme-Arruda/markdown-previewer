847a27cde6bcf54984397a4cfff665b7d2791c7b
Guilherme <guipanfiglio123@outlook.com>
2021-09-15 15:59:46 -0300

    Adding icons

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,18 +1,28 @@
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
+import { GitHub, GitBranch, Star } from "react-feather";
 
 export const NavBar = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="md">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand>Markdown Previewer</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar" />
         <Navbar.Collapse id="responsive-navbar">
           <Nav className="ms-auto">
-            <Nav.Link>Star this project</Nav.Link>
-            <Nav.Link>Fork to your GitHub</Nav.Link>
-            <Nav.Link>Follow @Guilherme-Arruda</Nav.Link>
+            <Nav.Link className="d-flex align-items-center">
+              <Star size={17} className="me-1" />
+              Star this project
+            </Nav.Link>
+            <Nav.Link className="d-flex align-items-center">
+              <GitBranch size={17} className="me-1" />
+              Fork to your GitHub
+            </Nav.Link>
+            <Nav.Link className="d-flex align-items-center">
+              <GitHub size={17} className="me-1" />
+              Follow @Guilherme-Arruda
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
