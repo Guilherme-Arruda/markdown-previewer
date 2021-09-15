abe74066281af1d7182e814c01c1da9a0fafe383
Guilherme <guipanfiglio123@outlook.com>
2021-09-14 23:07:52 -0300

    Update GlobalStyle.jsx

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -6,12 +6,4 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-
-  body {
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
 `;
