42531e6c0b0fcfccfaefc739ecb008a7e3cc7fc4
Guilherme <guipanfiglio123@outlook.com>
2021-09-16 15:58:12 -0300

    Final commit

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
+import { Output } from "../Output";
+import { markdown } from "../../Helpers/markdown";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
-import "./style.css";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export const Input = () => {
-  const [input, setInput] = useState();
+  const [input, setInput] = useState(markdown);
 
   const handleChange = (e) => {
     setInput(e.target.value);
@@ -15,6 +18,7 @@ export const Input = () => {
   const style = {
     resize: "none",
     height: "calc(100vh - 107px)",
+    fontFamily: "monospace",
   };
 
   return (
@@ -23,14 +27,21 @@ export const Input = () => {
         <Col className="p-0" sm={12} md={6}>
           <h6 className="text-center py-1 text-light bg-primary m-0">Editor</h6>
           <Form>
-            <Form.Control style={style} as="textarea" onChange={handleChange} />
+            <Form.Control
+              value={input}
+              style={style}
+              as="textarea"
+              onChange={handleChange}
+            />
           </Form>
         </Col>
         <Col className="p-0" md={6}>
           <h6 className="text-center py-1 text-light bg-primary m-0">
             Preview
           </h6>
-          <div className="output">{input}</div>
+          <Output>
+            <ReactMarkdown children={input} remarkPlugins={[remarkGfm]} />
+          </Output>
         </Col>
       </Row>
     </Container>
