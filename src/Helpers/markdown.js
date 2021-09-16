4edb9c5ab1b5d77150521eb2f70e9f1885fb818d
Guilherme <guipanfiglio123@outlook.com>
2021-09-16 15:56:46 -0300

    Create markdown.js

---
+++ src/Helpers/markdown.js
@@ -0,0 +1,74 @@
+export const markdown = `## Headings
+---
+
+# Heading h1
+## Heading h2 
+### Heading h3
+#### Heading h4
+##### Heading h5
+###### Heading h6
+
+## Emphasis
+---
+
+*This text will be italic*  
+_This will also be italic_
+
+**This text will be bold**  
+__This will also be bold__
+
+_You **can** combine them_
+
+## Lists
+---
+
+### Unordered
+
+* Item 1
+* Item 2
+* Item 3
+
+### Ordered
+
+1. Item 1
+1. Item 2
+1. Item 3
+
+## Images
+---
+
+![This is a alt text.](https://2.bp.blogspot.com/-AG1xE6WdtMY/UEtxjZNBCII/AAAAAAAAA4k/OXdLm9zR8f4/s1600/k-on_ritsu_icon.jpg)
+
+## Links
+---
+
+You may be using [Markdown Previewer](https://github.com/Guilherme-Arruda/markdown-previewer).
+
+## Blockquotes
+---
+
+> The world isn't perfect. But it's there for us, doing the best it can....that's what makes it so damn beautiful.
+>
+>> Don’t give up, there’s no shame in falling down! True shame is to not stand up again!
+
+## Tables
+---
+
+| Left columns | Right columns |
+| ------------ | ------------- |
+|   left foo   |   right foo   |
+|   left bar   |   right bar   |
+|   left baz   |   right baz   |
+
+## Blocks of code
+---
+
+\`\`\`
+let message = 'Hello world';
+alert(message);
+\`\`\`
+
+## Inline code
+---
+
+This web site is using \`react-markdown\`.`;
