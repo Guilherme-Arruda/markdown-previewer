d74941e54af80ec6735123b1bd417885591edb33
Guilherme <guipanfiglio123@outlook.com>
2021-09-16 16:15:22 -0300

    Update index.jsx

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Output } from "../Output";
-import { markdown } from "../../Helpers/markdown";
+import { markdown } from "../../helpers/markdown";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
